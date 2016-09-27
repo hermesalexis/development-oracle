555290909cb9656d6c33d69e2137b7e31680beff
hermesalexis <hermesagm@gmail.com>
2016-09-27 12:07:53 -0500

    Se mejoran los métodos para markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,24 +1,25 @@
 module ApplicationHelper
  # Initializes a Markdown parser
-	class CodeRayify < Redcarpet::Render::HTML
-	  def block_code(code, language)
-	    CodeRay.scan(code, language).div
-	  end
-	end
-
 	def markdown(text)
-	  coderayified = CodeRayify.new(filter_html: true, 
-	                                hard_wrap: true)
-	  options = {
-	     fenced_code_blocks: true,
-	     no_intra_emphasis: true,
-	     autolink: true,
-	     strikethrough: true,
-	     lax_html_blocks: true,
-	     superscript: true
-	  }
-	  markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
-	  markdown_to_html.render(text).html_safe
+	    options = {
+	      filter_html:     true,
+	      hard_wrap:       true, 
+	      link_attributes: { rel: 'nofollow', target: "_blank" },
+	      space_after_headers: true, 
+	      fenced_code_blocks: true,
+	      lax_spacing:true
+	    }
+
+	    extensions = {
+	      autolink:           true,
+	      superscript:        true,
+	      disable_indented_code_blocks: true
+	    }
+
+	    renderer = Redcarpet::Render::HTML.new(options)
+	    markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+	    markdown.render(text).html_safe
 	end
 	
 end
