939045aa17f435b03bbd7741127391854e8c6301
hermesalexis <hermesagm@gmail.com>
2016-09-28 12:54:06 -0500

    implementa ruta para answers

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :questions do 
   	resource :vote_question, only:[:create, :destroy]
   	resources :commentary_questions, only:[:create]
+  	resources :answers, only:[:create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
