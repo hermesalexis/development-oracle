917def06aeb5bab6a4eeeb72e4d707604ce0eea7
hermesalexis <hermesagm@gmail.com>
2016-10-01 12:51:12 -0500

    implementar formulario para comentarios de respuestas

--- app/controllers/commentary_answers_controller.rb
+++ app/controllers/commentary_answers_controller.rb
@@ -1,10 +1,9 @@
 class CommentaryAnswersController < ApplicationController
   before_action :authenticate_user!
+
  def create
   @commentary_answer = CommentaryAnswer.new(commentary_answer_params)
   @question = Question.find(params[:question_id])
-  @answer = Answer.find(params[:asnwer_id]) #lo debo mandar desde la vista
-  @commentary_answer.answer = @answer
 
   if @commentary_answer.save
    	redirect_to question_path(@question), notice:"El comentario ha sido creada correctamente"  
@@ -16,6 +15,6 @@ class CommentaryAnswersController < ApplicationController
 
  private
   def commentary_answer_params
-  	params.required(:commentary_answer).permit(:commentary).merge(user:current_user)
+  	params.required(:commentary_answer).permit(:commentary, :answer_id).merge(user:current_user)
   end
 end
